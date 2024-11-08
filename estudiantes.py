7677c18189ed2957a90fa10a0991f189f3fa80d8
BrendaSalas <l24200100@gmail.com>
2024-11-08 12:45:31 -0600

    se agregaron las propiedades self._carrera = "" y self._semestre = "" y sus respectivos getters y setters

--- estudiantes.py
+++ estudiantes.py
@@ -5,8 +5,22 @@ class Estudiantes(Persona):
     def __init__(self):
         super().__init__()
         self._matricula = ""
+        self._carrera = ""
+        self._semestre = ""
 
     # Getter y Setter
+    def get_carrera(self):
+        return self._carrera
+
+    def set_carrera(self, carrera):
+        self._carrera = carrera
+
+    def get_semestre(self):
+        return self._semestre
+
+    def set_semestre(self, semestre):
+        self._semestre = semestre
+
     def get_matricula(self):
         return self._matricula
 
@@ -15,4 +29,4 @@ class Estudiantes(Persona):
 
     def mostrar_informacion(self):
         base_info = super().mostrar_informacion()
-        return f"{base_info}, Matrícula: {self._matricula}"
+        return f"{base_info}, Matrícula: {self._matricula}, Carrera: {self._carrera}, Semestre: {self._semestre}"
